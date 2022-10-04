5ce43a1263af36b533bccf864ac7899aa0ea6819
invi1998 <2267661826@qq.com>
2022-10-04 10:41:34 +0800

    在多人会话子系统中添加一些会话功能以及委托和回调

--- Plugins/MultiplayerSession/Source/MultiplayerSession/Private/MultiplayerSessionSubsystem.cpp
+++ Plugins/MultiplayerSession/Source/MultiplayerSession/Private/MultiplayerSessionSubsystem.cpp
@@ -3,7 +3,12 @@
 #include "MultiplayerSessionSubsystem.h"
 #include "OnlineSubsystem.h"
 
-UMultiplayerSessionSubsystem::UMultiplayerSessionSubsystem()
+UMultiplayerSessionSubsystem::UMultiplayerSessionSubsystem() :
+	CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete)),
+	FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionComplete)),
+	JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete)),
+	DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete)),
+	StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
 {
 	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
 
@@ -13,4 +18,44 @@ UMultiplayerSessionSubsystem::UMultiplayerSessionSubsystem()
 		// 访问子系统并从中获得了会话接口并将其存储在私有变量 SessionInterface 中
 		// 现在就可以使用这些会话接口函数来管理我们的游戏会话
 	}
+}
+
+void UMultiplayerSessionSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+}
+
+void UMultiplayerSessionSubsystem::FindSession(int32 MaxSearchResults)
+{
+}
+
+void UMultiplayerSessionSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionSearchResult)
+{
+}
+
+void UMultiplayerSessionSubsystem::DestroySession()
+{
+}
+
+void UMultiplayerSessionSubsystem::StartSession()
+{
+}
+
+void UMultiplayerSessionSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnFindSessionComplete(bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
+{
 }
--- Plugins/MultiplayerSession/Source/MultiplayerSession/Public/MultiplayerSessionSubsystem.h
+++ Plugins/MultiplayerSession/Source/MultiplayerSession/Public/MultiplayerSessionSubsystem.h
@@ -19,8 +19,52 @@ class MULTIPLAYERSESSION_API UMultiplayerSessionSubsystem : public UGameInstance
 public:
 	UMultiplayerSessionSubsystem();
 
+	/// <summary>
+	/// 处理会话功能，菜单类会进行调用，可以从包含此子系统的任何类中调用他们
+	/// </summary>
+
+	// 创建会话并负责设置会话键值对
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+
+	/**
+	 * @brief 查找会话
+	 * @param MaxSearchResults 指定要搜索的结果数量
+	 */
+	void FindSession(int32 MaxSearchResults);
+
+	// 传入要加入的会话搜索结果，进而加入游戏会话
+	void JoinSession(const FOnlineSessionSearchResult& SessionSearchResult);
+
+	// 销毁会话
+	void DestroySession();
+
+	// 开始会话
+	void StartSession();
+
 protected:
+	// 将会被添加到在线会话接口委托列表里的内部回调函数
+	// 不需要在这个类外进行调用
+
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
 
 private:
 	IOnlineSessionPtr SessionInterface;		// 在线会话的智能指针
+
+	// 添加到在线会话界面委托列表中
+	// 我们 MultiPlayerSessionSubsystem 内部的回调函数 将会对这些委托进行绑定
+
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;			// 创建会话完成委托
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;			// 查找会话完成委托
+	FDelegateHandle FindSessionsCompleteDelegateHandle;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;				// 加入会话完成委托
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;		// 销毁会话完成委托
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;			// 开始会话完成委托
+	FDelegateHandle StartSessionCompleteDelegateHandle;
 };
